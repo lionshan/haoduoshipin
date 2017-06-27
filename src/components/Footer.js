6fe89f47f39062601d6b9f340a55e336ff9ddc7f
lionshan <1259179816@qq.com>
2017-06-27 11:20:28 +0800

    push

---
+++ src/components/Footer.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+class Footer extends React.Component {
+  render () {
+    let styles={
+      root:{
+        backgroundColor:'#212121',
+        textAlign:'center'
+      },
+      text:{
+        color:'#ccc',
+        lineHeight:'80px'
+      },
+      img:{
+        maxWidth:'200px'
+      },
+      wchat:{
+        lineHeight:'40px',
+        color:'#ccc'
+      }
+    }
+    return(
+      <div style={styles.root}>
+        <h1 style={styles.text}>越努力 越幸运</h1>
+        <p style={styles.wchat}>欢迎添加微信：newming1993</p>
+      </div>
+    )
+  }
+}
+
+// <img src="http://7xopqp.com1.z0.glb.clouddn.com/aboutme.jpg" title="me" style={styles.img}/>
+export default Footer;
